50d587a6b46dd222817978f20b8323f35c750fc5
annguyen0511 <nguyentrongan051102@gmail.com>
2024-12-04 19:04:34 +0700

    fix: footer component and add: detailproduct page

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -221,19 +221,19 @@
     <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
       <div>
         <p class="text-sm text-gray-700">
-          Showing
+          Hiển thị
           {{ ' ' }}
           <span class="font-medium">1</span>
           {{ ' ' }}
-          to
+          đến
           {{ ' ' }}
           <span class="font-medium">10</span>
           {{ ' ' }}
-          of
+          trong
           {{ ' ' }}
           <span class="font-medium">97</span>
           {{ ' ' }}
-          results
+          kết quả
         </p>
       </div>
       <div>
@@ -264,89 +264,16 @@
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <h2 class="text-center text-5xl font-bold text-rose-500">Thương Hiệu Nổi Bật</h2>
       <div class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-        <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="https://tailwindui.com/plus/img/logos/158x48/transistor-logo-gray-900.svg" alt="Transistor" width="158" height="48" />
-        <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="https://tailwindui.com/plus/img/logos/158x48/reform-logo-gray-900.svg" alt="Reform" width="158" height="48" />
-        <img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="https://tailwindui.com/plus/img/logos/158x48/tuple-logo-gray-900.svg" alt="Tuple" width="158" height="48" />
-        <img class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1" src="https://tailwindui.com/plus/img/logos/158x48/savvycal-logo-gray-900.svg" alt="SavvyCal" width="158" height="48" />
-        <img class="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1" src="https://tailwindui.com/plus/img/logos/158x48/statamic-logo-gray-900.svg" alt="Statamic" width="158" height="48" />
+        <img class="col-span-2 max-h-24 w-full object-contain lg:col-span-1" src="https://www.mykingdom.com.vn/cdn/shop/files/RESIZE_LOGO-01_260x.jpg?v=1712300444" alt="Transistor" width="158" height="48" />
+        <img class="col-span-2 max-h-24 w-full object-contain lg:col-span-1" src="https://www.mykingdom.com.vn/cdn/shop/files/RESIZE_LOGO-02_260x.jpg?v=1712300452" alt="Reform" width="158" height="48" />
+        <img class="col-span-2 max-h-24 w-full object-contain lg:col-span-1" src="https://www.mykingdom.com.vn/cdn/shop/files/BARBIE_-_236x120_cd63ec65-ba2d-4de7-adc4-bf5023f267a3_260x.png?v=1694144019" alt="Tuple" width="158" height="48" />
+        <img class="col-span-2 max-h-24 w-full object-contain sm:col-start-2 lg:col-span-1" src="https://www.mykingdom.com.vn/cdn/shop/files/236x120-05_260x.png?v=1731658485" alt="SavvyCal" width="158" height="48" />
+        <img class="col-span-2 col-start-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1" src="https://www.mykingdom.com.vn/cdn/shop/files/236x120-03_260x.png?v=1731658609" alt="Statamic" width="158" height="48" />
       </div>
     </div>
   </div>
   
-
-<footer class="bg-white dark:bg-gray-900">
-    <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-        <div class="md:flex md:justify-between">
-          <div class="mb-6 md:mb-0">
-              <a href="https://flowbite.com/" class="flex items-center">
-                  <img src="../assets/image/Logo.png" class="h-8 me-3" alt="FlowBite Logo" />
-                  <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Toy Toy World</span>
-              </a>
-          </div>
-          <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
-              <div>
-              </div>
-              <div>
-                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Theo dõi chúng tôi</h2>
-                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                      <li>
-                          <a href="https://discord.gg/h3eFpmaK" class="hover:underline">Discord</a>
-                      </li>
-                  </ul>
-              </div>
-              <div>
-                  <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Điều khoản</h2>
-                  <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                      <li class="mb-4">
-                          <a href="#" class="hover:underline">Privacy Policy</a>
-                      </li>
-                      <li>
-                          <a href="#" class="hover:underline">Terms &amp; Conditions</a>
-                      </li>
-                  </ul>
-              </div>
-          </div>
-      </div>
-      <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-      <div class="sm:flex sm:items-center sm:justify-between">
-          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="" class="hover:underline">Toy Toy World</a>. Đã đăng ký bản quyền.
-          </span>
-          <div class="flex mt-4 sm:justify-center sm:mt-0">
-              <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white">
-                  <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
-                        <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
-                    </svg>
-                  <span class="sr-only">Facebook page</span>
-              </a>
-              <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                  <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 21 16">
-                        <path d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z"/>
-                    </svg>
-                  <span class="sr-only">Discord community</span>
-              </a>
-              <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                  <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 17">
-                    <path fill-rule="evenodd" d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z" clip-rule="evenodd"/>
-                </svg>
-                  <span class="sr-only">Twitter page</span>
-              </a>
-              <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                  <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path fill-rule="evenodd" d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z" clip-rule="evenodd"/>
-                  </svg>
-                  <span class="sr-only">GitHub account</span>
-              </a>
-              <a href="#" class="text-gray-500 hover:text-gray-900 dark:hover:text-white ms-5">
-                  <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                    <path fill-rule="evenodd" d="M10 0a10 10 0 1 0 10 10A10.009 10.009 0 0 0 10 0Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.094 20.094 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM8 1.707a8.821 8.821 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.758 45.758 0 0 0 8 1.707ZM1.642 8.262a8.57 8.57 0 0 1 4.73-5.981A53.998 53.998 0 0 1 9.54 7.222a32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.64 31.64 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM10 18.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 13.113 11a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z" clip-rule="evenodd"/>
-                </svg>
-                  <span class="sr-only">Dribbble account</span>
-              </a>
-          </div>
-      </div>
-    </div>
-</footer>
-
+  <Footer/>
 
   <div class="bg-white">
     <div class="pt-6">
@@ -405,13 +332,13 @@
               </div>
               <p class="sr-only">{{ reviews.average }} out of 5 stars</p>
               <a :href="reviews.href" class="ml-3 text-sm font-medium text-indigo-600 hover:text-indigo-500">{{
-                reviews.totalCount }} reviews</a>
+                reviews.totalCount }} đánh giá</a>
             </div>
           </div>
 
           <form class="mt-10">
             <!-- Colors -->
-            <div>
+            <!-- <div>
               <h3 class="text-sm font-medium text-gray-900">Color</h3>
 
               <fieldset aria-label="Choose a color" class="mt-4">
@@ -425,16 +352,16 @@
                   </RadioGroupOption>
                 </RadioGroup>
               </fieldset>
-            </div>
+            </div> -->
 
             <!-- Sizes -->
-            <div class="mt-10">
+            <!-- <div class="mt-10">
               <div class="flex items-center justify-between">
                 <h3 class="text-sm font-medium text-gray-900">Size</h3>
                 <a href="#" class="text-sm font-medium text-indigo-600 hover:text-indigo-500">Size guide</a>
-              </div>
+              </div> -->
 
-              <fieldset aria-label="Choose a size" class="mt-4">
+              <!-- <fieldset aria-label="Choose a size" class="mt-4">
                 <RadioGroup v-model="selectedSize" class="grid grid-cols-4 gap-4 sm:grid-cols-8 lg:grid-cols-4">
                   <RadioGroupOption as="template" v-for="size in product.sizes" :key="size.name" :value="size"
                     :disabled="!size.inStock" v-slot="{ active, checked }">
@@ -455,7 +382,7 @@
                   </RadioGroupOption>
                 </RadioGroup>
               </fieldset>
-            </div>
+            </div> -->
 
             <button type="submit"
               class="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
@@ -512,10 +439,11 @@ import {
 import { Bars3Icon, MagnifyingGlassIcon, ShoppingBagIcon, XMarkIcon } from '@heroicons/vue/24/outline'
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/20/solid'
 import { RouterLink } from 'vue-router'
+import Footer from '../components/Footer.vue'
 
 const product = {
   name: 'ANGRY BIRDS Xe trớn tốc độ cao Angry Birds của chú chim nóng tính RED RED/AB23031',
-  price: '$192',
+  price: '49.000đ',
   href: '#',
   breadcrumbs: [
     { id: 1, name: 'Trang chủ', href: '#' },
@@ -565,7 +493,7 @@ const product = {
     'Thương hiệu: Angry Bird',
   ],
 }
-const reviews = { href: '#', average: 4, totalCount: 117 }
+const reviews = { href: '#', average: 4, totalCount: 57 }
 
 const selectedColor = ref(product.colors[0])
 const selectedSize = ref(product.sizes[2])
@@ -713,6 +641,14 @@ const products = [
     price: '419.000đ',
     imageSrc: 'https://www.mykingdom.com.vn/cdn/shop/files/do-choi-lap-rap-diy-xe-van-cap-cuu-co-den-va-am-thanh-vecto-vt8088c_1.jpg?v=1732246150&width=713',
     imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
+  },
+  {
+    id: 5,
+    name: 'Xe trớn tốc độ cao Red AngryBirds',
+    href: '#',
+    price: '45.000đ',
+    imageSrc: 'https://cdn.shopify.com/s/files/1/0731/6514/4343/products/AB23031_RED_1.jpg?v=1684929093&width=300',
+    imageAlt: 'Tall slender porcelain bottle with natural clay textured body and cork stopper.',
   },
   // More products...
 ]
